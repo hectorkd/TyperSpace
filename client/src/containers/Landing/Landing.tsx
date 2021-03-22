4f2770fe867c85543e54d810e214d095390ffbff
MarLukyanova <marlukyanova@gmail.com>
2021-03-22 11:50:57 +0000

    feat: client generate random roomId and redirect

--- client/src/containers/Landing/Landing.tsx
+++ client/src/containers/Landing/Landing.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Landing: React.FC = () => {
-  return <div>HELLO</div>;
+const history = useHistory();
+  //generate random string
+  function makeString(): string {
+    let outString = '';
+    const inOptions = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i++) {
+      outString += inOptions.charAt(Math.floor(Math.random() * inOptions.length));
+    }
+    console.log('generated random string', outString);
+    return outString;
+  }
+
+  //generate random roomID and redirect to avatar page
+  function handleClick(): void {
+    const roomId = makeString();
+    history.push(`/${roomId}`);
+  }
+
+
+  return (
+    <div>
+      {/* <Link to='/{makeString()}'>
+        Create Game
+      </Link> */}
+      <button type='button' onClick={handleClick}>
+        Create Game
+      </button>
+    </div>
+  );
 };
 
 export default Landing;
