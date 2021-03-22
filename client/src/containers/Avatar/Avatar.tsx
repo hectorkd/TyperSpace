66fd5d8842c24272a11bfbd190a340b553bcb444
MarLukyanova <marlukyanova@gmail.com>
2021-03-22 17:27:43 +0000

    fix: client ws connection send once

--- client/src/containers/Avatar/Avatar.tsx
+++ client/src/containers/Avatar/Avatar.tsx
@@ -1,29 +1,30 @@
-
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
 import SocketIOCLient from 'socket.io-client';
+// import useSocket from '../../useSocket';
 
 interface MatchParams {
   roomId: string;
 }
 
-const SOCKET_SERVER_URL = 'http://localhost:3001';
+const SOCKET_SERVER_URL = 'http://localhost:3001'; //TODO: keep in env
 
 const Avatar: React.FC<RouteComponentProps<MatchParams>> = (props) => {
-  // const socketRef = useRef();
+
+  // console.log('inside component with props', props);
   const roomId = props.match.params.roomId;
+  const socketRef = useRef<SocketIOClient.Socket>();
+  const [socket, setSocket] = useState(useRef<SocketIOClient.Socket>());
 
-  const socket = SocketIOCLient(SOCKET_SERVER_URL, {
-    query: {roomId}
-  });
-
-  // socket.on('connection', () => {
-  //   console.log(socket.id);
-  // })
+  useEffect(() => {
+    // const socketRef = useRef<SocketIOClient.Socket>();
+    socketRef.current = SocketIOCLient(SOCKET_SERVER_URL, {
+      query: {roomId}
+    });
+    setSocket(socketRef);
+  }, [roomId]);
 
   console.log('created socket', socket);
-
-  // console.log('created socket', socketRef);
 
   return <div>Hello Avatar</div>;
 
