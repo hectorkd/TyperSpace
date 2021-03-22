a5ccba643a9bf9baf20fa5d20dbbd412af724d39
MarLukyanova <marlukyanova@gmail.com>
2021-03-22 14:41:31 +0000

    feat: client first websocket connection

--- client/src/containers/Avatar/Avatar.tsx
+++ client/src/containers/Avatar/Avatar.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { RouteComponentProps } from 'react-router';
+import SocketIOCLient from 'socket.io-client';
 
-const Avatar: React.FC = () => {
-  return <div></div>;
+interface MatchParams {
+  roomId: string;
+}
+
+const SOCKET_SERVER_URL = 'http://localhost:3001';
+
+const Avatar: React.FC<RouteComponentProps<MatchParams>> = (props) => {
+  // const socketRef = useRef();
+  const roomId = props.match.params.roomId;
+
+  const socket = SocketIOCLient(SOCKET_SERVER_URL, {
+    query: {roomId}
+  });
+
+  // socket.on('connection', () => {
+  //   console.log(socket.id);
+  // })
+
+  console.log('created socket', socket);
+
+  // console.log('created socket', socketRef);
+
+  return <div>Hello Avatar</div>;
 };
 
 export default Avatar;
