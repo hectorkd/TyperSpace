a260f3f26fa9b632b11e9d78ed96ca262534da70
MarLukyanova <marlukyanova@gmail.com>
2021-03-22 10:59:15 +0000

    feat: client added routing

--- client/src/containers/Results/Results.tsx
+++ client/src/containers/Results/Results.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Results: React.FC = () => {
-  return <div></div>;
+  return <div>Hello Results</div>;
 };
 
 export default Results;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Landing from './containers/Landing/Landing';
+import Lobby from './containers/Lobby/Lobby';
+import Avatar from './containers/Avatar/Avatar';
+import Race from './containers/Race/Race';
+import Results from './containers/Results/Results';
+
 import './index.css';
-import Landing from './containers/Landing /Landing';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <Router>
+        <Route path="/" exact component={Landing} />
+        <Route exact path='/:roomId' component={Avatar} />
+        <Route exact path='/:roomId/lobby' component={Lobby}/>
+        <Route exact path='/:roomId/race' component={Race}/>
+        <Route exact path='/:roomId/results' component={Results} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
