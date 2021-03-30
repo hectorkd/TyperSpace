92472da879ff83e7482affd54ef2b30c7bec97d0
Gabor <ghalasz1988@gmail.com>
2021-03-30 10:52:05 +0100

    rearranged avatar page, waiting for carousel to restyle

--- src/containers/Avatar/Avatar.tsx
+++ src/containers/Avatar/Avatar.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState, ReactNode } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import SocketIOCLient from 'socket.io-client';
+
 import { FaCopy } from 'react-icons/fa';
 
 // const SOCKET_SERVER_URL = 'https://cryptic-fjord-92932.herokuapp.com/'; //TODO: keep in env
@@ -9,6 +10,7 @@ const SOCKET_SERVER_URL = 'http://localhost:3001'; //TODO: keep in env
 import rocketObj from '../../assets/icons/rocketObj';
 
 import './styles/Avatar.scss';
+
 import IPlayer from '../../interfaces/IPlayer';
 import { isConstructorDeclaration } from 'typescript';
 
@@ -111,73 +113,77 @@ const Avatar: React.FC<AvatarProps> = (props) => {
   //TODO: rocket selection: make it clear for user that he chose the rocket
   return (
     <div className="avatar-bg-container">
-      <div className="room-id-input input">
-        <label htmlFor="" className="input-label">
-          Race #
-        </label>
-        <div className="room-id-display">
-          <div className="room-id-text">{roomId}</div>
-          <button className="copy-button">
-            <FaCopy
-              className="copy-to-clipboard"
-              size="40px"
-              style={{ opacity: opacity }}
-              onClick={onCopy}
+      <div className="avatar-wrapper">
+        <div className="avatar-input-container">
+          <div className="room-id-input input">
+            <label htmlFor="" className="input-label">
+              Race #
+            </label>
+            <div className="room-id-display">
+              <div className="room-id-text">{roomId}</div>
+              <button className="copy-button">
+                <FaCopy
+                  className="copy-to-clipboard"
+                  size="40px"
+                  style={{ opacity: opacity }}
+                  onClick={onCopy}
+                />
+              </button>
+            </div>
+          </div>
+          <div className="name-field-input input">
+            <label htmlFor="" className="input-label">
+              enter name
+            </label>
+            <input
+              type="text"
+              value={userName}
+              maxLength={13}
+              onChange={(e) => setUserName(e.target.value)}
+              className="input-field"
             />
+          </div>
+          <div className="name-field-input input">
+            <label htmlFor="" className="input-label">
+              number of rounds
+            </label>
+            <input
+              type="text"
+              value={rounds}
+              onChange={(e) => setRounds(e.target.value)}
+              name=""
+              id=""
+              className="input-field"
+            />
+          </div>
+        </div>
+        <div className="avatar-select-container">
+          <h2 className="avatar-select-h1 ">select colour</h2>
+          <div className="avatar-list">
+            {Object.keys(selectedColors).map((color, idx) => {
+              return (
+                <img
+                  key={idx}
+                  id={color}
+                  src={rocketObj[`${color}Rocket`]}
+                  className={`avatar-images ${
+                    selectedColors[color]
+                      ? 'taken'
+                      : selectedColor === color
+                      ? 'selected'
+                      : ''
+                  }`}
+                  alt=""
+                  onClick={selectedColors[color] ? undefined : handleClick}
+                />
+              );
+            })}
+          </div>
+          <button className="btn-ready" onClick={handleClickReady}>
+            Ready
           </button>
         </div>
       </div>
-      <div className="name-field-input input">
-        <label htmlFor="" className="input-label">
-          enter name
-        </label>
-        <input
-          type="text"
-          value={userName}
-          maxLength={13}
-          onChange={(e) => setUserName(e.target.value)}
-          className="input-field"
-        />
-      </div>
-      <div className="name-field-input input">
-        <label htmlFor="" className="input-label">
-          number of rounds
-        </label>
-        <input
-          type="text"
-          value={rounds}
-          onChange={(e) => setRounds(e.target.value)}
-          name=""
-          id=""
-          className="input-field"
-        />
-      </div>
-      <div className="avatar-container">
-        <h2 className="avatar-select-h1 ">select colour</h2>
-        <div className="avatar-list">
-          {Object.keys(selectedColors).map((color, idx) => {
-            return (
-              <img
-                key={idx}
-                id={color}
-                src={rocketObj[`${color}Rocket`]}
-                className={`avatar-images ${
-                  selectedColors[color]
-                    ? 'taken'
-                    : selectedColor === color
-                    ? 'selected'
-                    : ''
-                }`}
-                alt=""
-                onClick={selectedColors[color] ? undefined : handleClick}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <button className="btn-ready" onClick={handleClickReady}>
-        Ready
-      </button>
     </div>
   );
 };
